82b3de5f2bf172ab0e690e7a1657c17abb97217a
Zangura <zanguraa@gmail.com>
2023-11-27 23:37:14 +0400

    the project CharCounter is finished

---
+++ CharCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CharCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Please enter phrase: ");
+            var phrase = Console.ReadLine().Split(' ').ToArray();
+            CharCounter(phrase);
+
+        }
+        public static void CharCounter(Array phrase)
+        {
+            var counter = 0;
+            foreach (var word in phrase)
+            {
+                foreach (var letter in word.ToString())
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"The phrase has {counter} characters");
+        }
+    }
+}
