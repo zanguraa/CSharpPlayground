8e5569c557335ed860c81f7a150f0008ed64d4ef
Zangura <zanguraa@gmail.com>
2023-11-27 22:53:59 +0400

    added array reverse project

---
+++ ArrayReverse/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ArrayReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter word: ");
+            var word = Console.ReadLine();
+            var reversedWord = Reverse(word);
+            Console.WriteLine();
+            Console.WriteLine($"Reversed word is: {reversedWord}");
+        }
+        public static string Reverse(string word)
+        {
+            var reversedWord = "";
+            for (var i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+            return reversedWord;
+        }
+    }
+}
