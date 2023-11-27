f4a138cb49bf715e5ccc6f8a7f9ae41c710f9d37
Zangura <zanguraa@gmail.com>
2023-11-27 19:40:23 +0400

    added average of five and more or less numbers project

---
+++ AvgOfFiveNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AvgOfFiveNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHello, This is console app for calculated numbers average!");
+            Console.WriteLine("Please enter numbers with colon (5, 55, 34), or use spaces between numbers: ");
+            double[] numbersArray = Console.ReadLine().Split(',', ' ').Select(double.Parse).ToArray();
+
+            double average = AverageOfNumbers(numbersArray);
+            Console.WriteLine($"\nThe average number is: {average}");
+        }
+        public static double AverageOfNumbers(double[] numbers)
+        {
+            double sum = 0;
+            foreach (double number in numbers)
+            {
+                sum += number;
+            }
+            return sum / numbers.Length;
+        }
+    }
+}
