8089b28fe4574713f23d18be1a98c5282c64a739
Zangura <zanguraa@gmail.com>
2023-12-19 00:54:14 +0400

    added ArrayOrderBy project

---
+++ ArrayOrderBy/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ArrayOrderBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 45, 3, 22, 43, 1, 0, 4 };
+
+            OrderArray(numbers);
+
+
+        }
+
+        static void OrderArray(int[] numbers)
+        {
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
