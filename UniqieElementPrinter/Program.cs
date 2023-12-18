53511a607da9f9ec8a46af322115ba50682ce09b
Zangura <zanguraa@gmail.com>
2023-12-19 00:34:15 +0400

    added IsUniqueElementPrinter project

---
+++ UniqieElementPrinter/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace UniqieElementPrinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 1, 2, 3, 4, 5 };
+            PrintUniqueElement(array);
+        }
+        static void PrintUniqueElement(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                bool isUnique = true;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j] && i != j)
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+
+            }
+        }
+    }
+}
