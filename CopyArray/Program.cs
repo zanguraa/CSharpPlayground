a780b6babf2912e7eeee7ee34a487abb44c89558
Zangura <zanguraa@gmail.com>
2023-12-19 00:02:32 +0400

    added project CopyArray from one array to second array

---
+++ CopyArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CopyArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = { 1, 2, 3, 4, 5 };
+            CopyArray(intNumbers);
+
+        }
+        static void CopyArray(int[] intNumbers)
+        {
+            int[] newIntNumbersArray = new int[intNumbers.Length];
+            for (int i = 0; i < intNumbers.Length; i++)
+            {
+                newIntNumbersArray[i] = intNumbers[i];
+            }
+            for (int i = 0; i < newIntNumbersArray.Length; i++)
+            {
+                Console.WriteLine(newIntNumbersArray[i]);
+            }
+        }
+    }
+}
