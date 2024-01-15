cf5099c31cc60016f5587ae759ec8e7a75aee637
Zangura <zanguraa@gmail.com>
2024-01-15 23:20:44 +0400

    added Products and Sales classes, also Management class with connection databes. connection is via dapper

---
+++ InventoryManagementSystem/InventoryManager.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public class InventoryManager
+    {
+        private readonly string connectionString;
+
+        public InventoryManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void AddProduct(Product product)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute("INSERT INTO Products (Name, Price, Stock) VALUES (@ProductName, @Price, @Stock)", product);
+            }
+        }
+
+        public Product GetProduct(int productId)
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirstOrDefault<Product>("SELECT * FROM Products WHERE Id = @ProductId", new { ProductId = productId });
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts ()
+        {
+            using (var connection = new SqlConnection (connectionString))
+            {
+                connection.Open();
+                return connection.Query<Product>("SELECT * FROM Products");
+            }
+        }
+
+        public void RecordSale(Product product, int quantitySold)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                if (product.Stock >= quantitySold)
+                {
+                    product.Stock -= quantitySold;
+                    connection.Execute("UPDATE Products SET Stock = @Stock WHERE Id = @ProductId", new { Stock = product.Stock, ProductId = product.Id });
+
+                    Sales sale = new Sales
+                    {
+                        ProductId = product.Id,
+                        Quantity = quantitySold,
+                        SaleDate = DateTime.Now,
+                    };
+
+                    connection.Execute("INSERT INTO Sales (ProductId, Quantity, SaleDate) VALUES (@ProductId, @Quantity, @SaleDate)", sale);
+                } else
+                {
+                    Console.WriteLine("Not enought stock available for the sale");
+                }
+            }
+        }
+
+    }
+}
--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace InventoryManagementSystem
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Metrics;
+using Dapper;
+
+namespace InventoryManagementSystem
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+           InventoryManager inventoryManager = new InventoryManager("Data Source=DESKTOP-7OLVUTI;Initial Catalog=InventoryDB;Integrated Security=SSPI;TrustServerCertificate=True");
+            
+            Product newProduct = new Product
+            {
+                ProductName = "Xiaomi  13 Pro",
+                Price = 637.35m,
+                Stock = 45
+            };
+            
+            inventoryManager.AddProduct(newProduct);
+            Console.WriteLine("Product was added successfully.");
+
+            int ProductItToRetrieve = 2;
+
+            Product ProductToSell = inventoryManager.GetProduct(ProductItToRetrieve);
+
+
+            if(ProductToSell != null)
+            {
+                Console.WriteLine($"Initial stock of {ProductToSell.ProductName}: {ProductToSell.Stock}");
+                int quantitySold = 34;
+                inventoryManager.RecordSale(ProductToSell, quantitySold);
+                Console.WriteLine($"Uptadet stock of {ProductToSell.ProductName}: {ProductToSell.Stock}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Product with id: {ProductItToRetrieve} not found.");
+            }
+
+            var allProducts = inventoryManager.GetAllProducts();
+
+            foreach (var product in allProducts)
+            {
+                Console.WriteLine($"Product ID: {product.Id}, Name: {product.ProductName}, Price: {product.Price}, Stock: {product.Stock}");
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
